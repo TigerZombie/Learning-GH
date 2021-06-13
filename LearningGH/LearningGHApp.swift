925368c7468faaae8d9cb2c6fe65a2146183668e
Hendrik Jahns <hendrik.jahns@outlook.com>
2021-06-13 19:17:58 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Hendrik Jahns on 13.06.21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
